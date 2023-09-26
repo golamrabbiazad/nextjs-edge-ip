0d9daf5c03655026375041859940b5b95dee198e
Golamrabbi Azad <35021384+golamrabbiazad@users.noreply.github.com>
2023-09-27 05:03:47 +0600

    fix: let's see

--- app/api/get-ip/route.ts
+++ app/api/get-ip/route.ts
@@ -1,11 +1,14 @@
 import { NextResponse, NextRequest, userAgent } from "next/server";
-
+import { headers } from "next/headers";
 export async function GET(req: NextRequest) {
   const { ua } = userAgent(req);
 
+  const headersList = headers();
+  const ip = headersList.get("x-forwarded-for");
+
   const data = {
     ok: true,
-    ip_address: req.ip,
+    ip_address: ip,
     user_agent: ua,
   };
 
