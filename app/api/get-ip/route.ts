2da63695481e16ee2891fcd0140e8eff6c552f17
Golamrabbi Azad <35021384+golamrabbiazad@users.noreply.github.com>
2023-09-27 04:54:31 +0600

    fix: natively find ip address

--- app/api/get-ip/route.ts
+++ app/api/get-ip/route.ts
@@ -1,17 +1,11 @@
 import { NextResponse, NextRequest, userAgent } from "next/server";
 
 export async function GET(req: NextRequest) {
-  const response = await fetch("https://geolocation-db.com/json/");
-  if (!response.ok) {
-    console.error("failed to fetch");
-  }
-
-  const { IPv4 } = await response.json();
   const { ua } = userAgent(req);
 
   const data = {
     ok: true,
-    ip_address: IPv4,
+    ip_address: req.ip,
     user_agent: ua,
   };
 
